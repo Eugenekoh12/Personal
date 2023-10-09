3981ea5f4bbb088a89f5727397535fc3217acef0
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-10-10 06:53:46 +0800

    Update AILRCIBTWII on whitemoonxblacksun.ca.meta.js
    Preparing to Move & Archive

--- personal/userscripts/Replace Text/Websites/LN&Stories/AILRCIBTWII on whitemoonxblacksun.ca.meta.js
+++ personal/userscripts/Replace Text/Websites/LN&Stories/AILRCIBTWII on whitemoonxblacksun.ca.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Text - whitemoonxblacksun.ca - AILRCIBTWII
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1.0.3.9
+// @version      1.0.4
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text for AILRCIBTWII on whitemoonxblacksun.ca.
 // @author       Eugenekoh12
 // @updateURL    https://eugenekoh12.github.io/personal/userscripts/Replace%20Text/Websites/LN%26Stories/AILRCIBTWII%20on%20whitemoonxblacksun.ca.meta.js
